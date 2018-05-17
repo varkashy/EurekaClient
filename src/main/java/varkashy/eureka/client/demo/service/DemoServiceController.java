c0cb3d6281a9caa6099728d48f58a5f038d72094
varkashy <varunkkashyap@gmail.com>
2018-05-16 19:20:01 -0500

    eurekaClient

---
+++ src/main/java/varkashy/eureka/client/demo/service/DemoServiceController.java
@@ -0,0 +1,22 @@
+package varkashy.eureka.client.demo.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoServiceController {
+
+    @Value("${words}")
+    String words;
+
+    @GetMapping("/words")
+    public @ResponseBody
+    String getWord() {
+        String[] wordArray = words.split(",");
+        int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+        return wordArray[i];
+    }
+
+}
